de72a0e057f187e9c7d7b4363a7853bd8ad1cc9c
Tömördi Péter <tomordi.peter@zengo.eu>
2022-04-15 13:43:34 +0200

    auto anim

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 let carousel;
+let carouselAnimIntervalId;
+let carouselDelayStartTimerId;
+const animTime = 4000;
+let actSlide = 1;
+const maxSlide = 6;
+let slideIdPrefix = '#carousel__slide';
+
 window.onload = () => {
-  let parent = document.getElementById('headerContainer');
-  let header = document.getElementById('headerContent');
-  document.addEventListener('scroll', () => {
-	  const parentRect = parent.getBoundingClientRect();
-	  if (parentRect.y < -5) {  
-		header.parentElement.classList.add('header-fixed');
-	  } else {
-		  header.parentElement.classList.remove('header-fixed');
-	  }
-  });
-  carousel = document.getElementsByClassName('carousel')[0];
-  carousel.style.height = window.innerHeight;
+  setHeaderStyles();
+  setCarouselHeight();
+  
+  carouselAnimIntervalId = window.setInterval(animateCarousel, animTime);
+  const nextButtons = document.getElementsByClassName('carousel__next');
+  
+  
 }
 
 window.onResize = () => {
 	carousel.style.height = window.innerHeight;
+}
+
+function setCarouselHeight() {
+	carousel = document.getElementsByClassName('carousel')[0];
+	carousel.style.height = window.innerHeight;
+}
+
+function setHeaderStyles() {
+	let parent = document.getElementById('headerContainer');
+	let header = document.getElementById('headerContent');
+	document.addEventListener('scroll', () => {
+		const parentRect = parent.getBoundingClientRect();
+		if (parentRect.y < -5) {  
+			header.parentElement.classList.add('header-fixed');
+			window.clearInterval(carouselAnimIntervalId);
+			carouselAnimIntervalId = NaN;
+		} else {
+			header.parentElement.classList.remove('header-fixed');
+			if (isNaN(carouselAnimIntervalId)) carouselAnimIntervalId = window.setInterval(animateCarousel, animTime);
+		}
+	});
+}
+
+function animateCarousel() {
+	actSlide = actSlide + 1 > maxSlide ? 1 : actSlide + 1;
+	location.href = slideIdPrefix + actSlide;
+}
+
+function pauseAndRestartCarouselAnim() {
+	window.clearInterval(carouselAnimIntervalId);
+	carouselAnimIntervalId = NaN;
+	window.clearTimeout(carouselDelayStartTimerId);
+	carouselDelayStartTimerId = window.setTimeout(() => {
+		if (isNaN(carouselAnimIntervalId)) carouselAnimIntervalId = window.setInterval(animateCarousel, animTime);
+	}, animTime);
 }
